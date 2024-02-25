45ca9239a2154bb4c1dfac36cd4c202eaf32de6b
Jappie Klooster <jappieklooster@hotmail.com>
2024-02-25 01:09:41 +0100

    fix warnings

--- src/Database/Esqueleto/Postgis.hs
+++ src/Database/Esqueleto/Postgis.hs
@@ -1,44 +1,48 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 -- | Haskell bindings for postgres postgis
 --   for a good explenation see <https://postgis.net/>
-module Database.Esqueleto.Postgis (
-  st_contains,
-  st_point,
-  -- st_polygon
-  PostgisGeometry(..)
-  , makePolygon
-) where
+module Database.Esqueleto.Postgis
+  ( PostgisGeometry (..),
+    makePolygon,
 
-import Data.Sequence qualified as Seq
-import Data.Sequence(Seq(..), (|>))
-import Data.Geospatial(GeospatialGeometry, GeoPoint (..))
-import Data.Ewkb(parseHexByteString)
-import Data.Hex(Hex(..))
-import Database.Esqueleto.Experimental(SqlExpr, Value)
-import Database.Persist.Sql
-import Database.Esqueleto.Internal.Internal(unsafeSqlFunction)
-import Data.Text(pack, Text)
-import Data.Bifunctor(first)
-import Data.LineString (LineString)
-import GHC.Base (NonEmpty)
-import Data.LinearRing (LinearRing, makeLinearRing, toSeq)
-import Data.Geospatial.Internal.BasicTypes (PointXY)
-import Data.Geospatial (GeoPositionWithoutCRS)
-import Data.Geospatial (GeoPositionWithoutCRS(..))
-import Data.Geospatial (PointXYZM)
-import Data.Geospatial (PointXYZ)
+    -- * functions
+    st_contains,
+    st_intersects,
+
+    -- * points
+    st_point,
+    st_point_xyz,
+    st_point_xyzm,
+  )
+where
+
+import Data.Bifunctor (first)
+import Data.Ewkb (parseHexByteString)
+import Data.Foldable (Foldable (toList), fold)
+import Data.Geospatial (GeoPoint (..), GeoPositionWithoutCRS (..), GeospatialGeometry, PointXY (..), PointXYZ, PointXYZM)
 import Data.Geospatial qualified as Geospatial
-import Data.Geospatial (PointXY(..))
-import qualified Data.Text.Lazy.Builder as Text
-import Data.String (IsString(..))
-import Data.Text.Lazy.Builder (toLazyText)
-import Data.Text.Lazy (toStrict)
-import qualified Data.List.NonEmpty as Non
-import Data.Foldable (fold, Foldable (toList))
+import Data.Hex (Hex (..))
+import Data.LineString (LineString)
+import Data.LinearRing (LinearRing, makeLinearRing, toSeq)
+import Data.List qualified as List
 import Data.List.NonEmpty (nonEmpty)
-import qualified Data.List as List
+import Data.List.NonEmpty qualified as Non
+import Data.Sequence (Seq (..), (|>))
+import Data.Sequence qualified as Seq
+import Data.String (IsString (..))
+import Data.Text (Text, pack)
+import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text.Lazy.Builder qualified as Text
+import Database.Esqueleto.Experimental (SqlExpr, Value)
+import Database.Esqueleto.Internal.Internal (unsafeSqlFunction)
+import Database.Persist.Sql
+import GHC.Base (NonEmpty)
+import Data.Geospatial (PointXYZM(..))
+import Data.Geospatial (PointXYZ(..))
 
 tshow :: Show a => a -> Text
 tshow = pack . show
@@ -46,37 +50,39 @@ tshow = pack . show
 -- | like 'GeospatialGeometry' but not partial, eg no empty geometries, also only works
 --   in a single dimention, eg PostgisGeometry PointXY can't work with PostgisGeometry PointXYZ.
 --   so PointXY indicates a 2 dimension space, and PointXYZ a three dimension space.
-data PostgisGeometry point = Point point
-                      | MultiPoint (NonEmpty point )
-                      | Line (LineString point )
-                      | Multiline (NonEmpty (LineString point))
-                      | Polygon (LinearRing point )
-                      | MultiPolygon (NonEmpty (LinearRing point ))
-                      | Collection (NonEmpty (PostgisGeometry point))
-                      deriving (Show, Functor, Eq)
+data PostgisGeometry point
+  = Point point
+  | MultiPoint (NonEmpty point)
+  | Line (LineString point)
+  | Multiline (NonEmpty (LineString point))
+  | Polygon (LinearRing point)
+  | MultiPolygon (NonEmpty (LinearRing point))
+  | Collection (NonEmpty (PostgisGeometry point))
+  deriving (Show, Functor, Eq)
 
-data GeomErrors = MismatchingDimensionsXYZ PointXYZ
-                | MismatchingDimensionsXYZM PointXYZM
-                | NoGeometry
-                | EmptyPoint
-                | NotImplemented
-                | EmptyMultiline
-                | EmptyMultiPoint
-                | NotEnoughElements (Seq PointXY)
-                | EmptyMultipolygon
-                | EmptyCollection
-                deriving Show
+data GeomErrors
+  = MismatchingDimensionsXYZ PointXYZ
+  | MismatchingDimensionsXYZM PointXYZM
+  | MismatchingDimensionsXY PointXY
+  | NoGeometry
+  | EmptyPoint
+  | NotImplemented
+  | EmptyMultiline
+  | EmptyMultiPoint
+  | NotEnoughElements
+  | EmptyMultipolygon
+  | EmptyCollection
+  deriving (Show)
 
 -- | checks if the first point is the last, and if not so makes it so.
 --   this is required for inserting into the database
 makePolygon :: (Eq point, Show point) => point -> point -> point -> Seq point -> LinearRing point
 makePolygon one two three other =
-  if Just one == last then
-    makeLinearRing  one two three other
-  else
-    makeLinearRing  one two three (other |> one)
+  if Just one == last'
+    then makeLinearRing one two three other
+    else makeLinearRing one two three (other |> one)
   where
-    last = Seq.lookup (length other) other
+    last' = Seq.lookup (length other) other
 
 from2dGeoPositionWithoutCRSToPoint :: GeoPositionWithoutCRS -> Either GeomErrors PointXY
 from2dGeoPositionWithoutCRSToPoint = \case
@@ -85,72 +91,137 @@ from2dGeoPositionWithoutCRSToPoint = \case
   GeoPointXYZ x -> Left (MismatchingDimensionsXYZ x)
   GeoPointXYZM x -> Left (MismatchingDimensionsXYZM x)
 
+from3dGeoPositionWithoutCRSToPoint :: GeoPositionWithoutCRS -> Either GeomErrors PointXYZ
+from3dGeoPositionWithoutCRSToPoint = \case
+  GeoEmpty -> Left EmptyPoint
+  GeoPointXY x -> Left (MismatchingDimensionsXY x)
+  GeoPointXYZ x -> Right x
+  GeoPointXYZM x -> Left (MismatchingDimensionsXYZM x)
+
+from4dGeoPositionWithoutCRSToPoint :: GeoPositionWithoutCRS -> Either GeomErrors PointXYZM
+from4dGeoPositionWithoutCRSToPoint = \case
+  GeoEmpty -> Left EmptyPoint
+  GeoPointXY x -> Left (MismatchingDimensionsXY x)
+  GeoPointXYZ x -> Left (MismatchingDimensionsXYZ x)
+  GeoPointXYZM x -> Right x
+
 renderPair :: PointXY -> Text.Builder
 renderPair (PointXY {..}) = fromString (show _xyX) <> " " <> fromString (show _xyY)
+
+
+renderXYZ :: PointXYZ -> Text.Builder
+renderXYZ (PointXYZ {..}) = fromString (show _xyzX) <> " " <> fromString (show _xyzY) <> " " <> fromString (show _xyzZ)
+
+
+renderXYZM :: PointXYZM -> Text.Builder
+renderXYZM (PointXYZM {..}) = fromString (show _xyzmX) <> " " <> fromString (show _xyzmY) <> " " <> fromString (show _xyzmZ) <> " " <> fromString (show _xyzmM)
+
 
 renderGeometry :: PostgisGeometry Text.Builder -> Text.Builder
 renderGeometry = \case
   Point point -> "POINT(" <> point <> ")"
-  MultiPoint points -> "MULTIPOINT (" <> fold (Non.intersperse "," ((\x -> "(" <> x <> ")" ) <$> points)) <> ")"
+  MultiPoint points -> "MULTIPOINT (" <> fold (Non.intersperse "," ((\x -> "(" <> x <> ")") <$> points)) <> ")"
   Line line -> "LINESTRING(" <> renderLines line <> ")"
-  Multiline (multiline) -> "MULTILINESTRING(" <> fold (Non.intersperse "," ((\ line -> "(" <> renderLines line <> ")") <$> multiline)) <>")"
+  Multiline (multiline) -> "MULTILINESTRING(" <> fold (Non.intersperse "," ((\line -> "(" <> renderLines line <> ")") <$> multiline)) <> ")"
   Polygon polygon -> "POLYGON((" <> renderLines polygon <> "))"
-  MultiPolygon multipolygon -> "MULTIPOLYGON(" <> fold (Non.intersperse "," ((\ line -> "((" <> renderLines line <> "))") <$> multipolygon))<> ")"
-  Collection collection -> "GEOMETRYCOLLECTION("<> fold (Non.intersperse "," (renderGeometry <$> collection)) <>  ")"
+  MultiPolygon multipolygon -> "MULTIPOLYGON(" <> fold (Non.intersperse "," ((\line -> "((" <> renderLines line <> "))") <$> multipolygon)) <> ")"
+  Collection collection -> "GEOMETRYCOLLECTION(" <> fold (Non.intersperse "," (renderGeometry <$> collection)) <> ")"
 
 renderLines :: Foldable f => f Text.Builder -> Text.Builder
 renderLines line = fold (List.intersperse "," $ toList line)
 
-from2dGeospatialGeometry :: GeospatialGeometry -> Either GeomErrors (PostgisGeometry PointXY)
-from2dGeospatialGeometry = \case
+from2dGeospatialGeometry :: (Eq a, Show a) => (GeoPositionWithoutCRS -> Either GeomErrors a) -> GeospatialGeometry -> Either GeomErrors (PostgisGeometry a)
+from2dGeospatialGeometry interpreter = \case
   Geospatial.NoGeometry -> Left NoGeometry
-  Geospatial.Point (GeoPoint point) -> (Point <$> from2dGeoPositionWithoutCRSToPoint point)
+  Geospatial.Point (GeoPoint point) -> (Point <$> interpreter point)
   Geospatial.MultiPoint (Geospatial.GeoMultiPoint points) -> do
-    list' <- sequence $ toList (from2dGeoPositionWithoutCRSToPoint <$> points)
+    list' <- sequence $ toList (interpreter <$> points)
     case nonEmpty list' of
       Nothing -> Left EmptyMultiPoint
       Just x -> Right $ MultiPoint x
-  Geospatial.Line (Geospatial.GeoLine linestring) -> Line <$> traverse from2dGeoPositionWithoutCRSToPoint linestring
+  Geospatial.Line (Geospatial.GeoLine linestring) -> Line <$> traverse interpreter linestring
   Geospatial.MultiLine (Geospatial.GeoMultiLine multiline) -> do
-    seqRes <- traverse (traverse from2dGeoPositionWithoutCRSToPoint) multiline
+    seqRes <- traverse (traverse interpreter) multiline
     case Non.nonEmpty (toList seqRes) of
-      Just nonEmpty -> Right $ Multiline nonEmpty
+      Just nonEmpty' -> Right $ Multiline nonEmpty'
       Nothing -> Left EmptyMultiline
-  Geospatial.Polygon (Geospatial.GeoPolygon polygon) -> Polygon <$> toLinearRing polygon
+  Geospatial.Polygon (Geospatial.GeoPolygon polygon) -> Polygon <$> (toLinearRing interpreter) polygon
   Geospatial.MultiPolygon (Geospatial.GeoMultiPolygon multipolygon) -> do
-    seqRings <- traverse toLinearRing multipolygon
-    case Non.nonEmpty (toList seqRings ) of
-      Just nonEmpty -> Right $ MultiPolygon nonEmpty
+    seqRings <- traverse (toLinearRing interpreter) multipolygon
+    case Non.nonEmpty (toList seqRings) of
+      Just nonEmpty' -> Right $ MultiPolygon nonEmpty'
       Nothing -> Left EmptyMultipolygon
-
-  Geospatial.Collection seq -> do
-    seqs <- traverse from2dGeospatialGeometry seq
-    case Non.nonEmpty (toList seqs ) of
-      Just nonEmpty -> Right $ Collection nonEmpty
+  Geospatial.Collection seq' -> do
+    seqs <- traverse (from2dGeospatialGeometry interpreter) seq'
+    case Non.nonEmpty (toList seqs) of
+      Just nonEmpty' -> Right $ Collection nonEmpty'
       Nothing -> Left EmptyCollection
 
-toLinearRing ::  Seq (LinearRing GeoPositionWithoutCRS) -> Either GeomErrors (LinearRing PointXY)
-toLinearRing polygon = do
-    aSeq <- traverse from2dGeoPositionWithoutCRSToPoint (foldMap toSeq polygon)
-    case aSeq of
-      (one :<| two :<| three :<| rem) -> Right $ makeLinearRing  one two three rem
-      other -> Left (NotEnoughElements other)
+
+toLinearRing :: (Eq a, Show a) => (GeoPositionWithoutCRS -> Either GeomErrors a) -> Seq (LinearRing GeoPositionWithoutCRS) -> Either GeomErrors (LinearRing a)
+toLinearRing interpreter polygon = do
+  aSeq <- traverse interpreter (foldMap toSeq polygon)
+  case aSeq of
+    (one :<| two :<| three :<| rem') -> Right $ makeLinearRing one two three rem'
+    _other -> Left NotEnoughElements
 
 instance PersistField (PostgisGeometry PointXY) where
   toPersistValue geom =
-      PersistText $ toStrict $ toLazyText $ renderGeometry $ renderPair <$> geom
+    PersistText $ toStrict $ toLazyText $ renderGeometry $ renderPair <$> geom
   fromPersistValue (PersistLiteral_ Escaped bs) = do
-      result <- first pack $ parseHexByteString (Hex bs)
-      first tshow $ from2dGeospatialGeometry result
+    result <- first pack $ parseHexByteString (Hex bs)
+    first tshow $ (from2dGeospatialGeometry from2dGeoPositionWithoutCRSToPoint) result
+  fromPersistValue other = Left ("PersistField.Polygon: invalid persist value:" <> tshow other)
+
+instance PersistField (PostgisGeometry PointXYZ) where
+  toPersistValue geom =
+    PersistText $ toStrict $ toLazyText $ renderGeometry $ renderXYZ <$> geom
+  fromPersistValue (PersistLiteral_ Escaped bs) = do
+    result <- first pack $ parseHexByteString (Hex bs)
+    first tshow $ (from2dGeospatialGeometry from3dGeoPositionWithoutCRSToPoint) result
+  fromPersistValue other = Left ("PersistField.Polygon: invalid persist value:" <> tshow other)
+
+instance PersistField (PostgisGeometry PointXYZM) where
+  toPersistValue geom =
+    PersistText $ toStrict $ toLazyText $ renderGeometry $ renderXYZM <$> geom
+  fromPersistValue (PersistLiteral_ Escaped bs) = do
+    result <- first pack $ parseHexByteString (Hex bs)
+    first tshow $ (from2dGeospatialGeometry from4dGeoPositionWithoutCRSToPoint) result
   fromPersistValue other = Left ("PersistField.Polygon: invalid persist value:" <> tshow other)
 
 instance PersistFieldSql (PostgisGeometry PointXY) where
   sqlType _ = SqlOther "geometry"
 
-st_contains :: SqlExpr (Value (PostgisGeometry a)) -> SqlExpr (Value (PostgisGeometry a)) -> SqlExpr (Value Bool)
-st_contains a b = unsafeSqlFunction "ST_CONTAINS" (a,b)
+instance PersistFieldSql (PostgisGeometry PointXYZ) where
+  sqlType _ = SqlOther "geometry"
+
+instance PersistFieldSql (PostgisGeometry PointXYZM) where
+  sqlType _ = SqlOther "geometry"
+
+-- | Returns TRUE if geometry A contains geometry B.
+--   https://postgis.net/docs/ST_Contains.html
+st_contains ::
+  -- | geom a
+  SqlExpr (Value (PostgisGeometry a)) ->
+  -- | geom b
+  SqlExpr (Value (PostgisGeometry a)) ->
+  SqlExpr (Value Bool)
+st_contains a b = unsafeSqlFunction "ST_CONTAINS" (a, b)
+
+-- | Returns true if two geometries intersect.
+--   Geometries intersect if they have any point in common.
+--   https://postgis.net/docs/ST_Intersects.html
+st_intersects ::
+  SqlExpr (Value (PostgisGeometry a)) ->
+  SqlExpr (Value (PostgisGeometry a)) ->
+  SqlExpr (Value Bool)
+st_intersects a b = unsafeSqlFunction "ST_Intersects" (a, b)
 
 st_point :: SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value (PostgisGeometry PointXY))
-st_point a b = unsafeSqlFunction "ST_POINT" (a,b)
+st_point a b = unsafeSqlFunction "ST_POINT" (a, b)
 
--- st_polygon
+st_point_xyz :: SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value (PostgisGeometry PointXYZ))
+st_point_xyz a b c = unsafeSqlFunction "ST_POINT" (a, b, c)
+
+st_point_xyzm :: SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value Double) -> SqlExpr (Value (PostgisGeometry PointXYZM))
+st_point_xyzm a b c m = unsafeSqlFunction "ST_POINT" (a, b, c, m)
--- test/Test.hs
+++ test/Test.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -15,77 +15,70 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-
 module Main where
 
-import Test.Tasty
-import Test.Tasty.HUnit
-
-import Data.List(sort)
-import Database.Esqueleto.Postgis
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
-import Hedgehog
-import Control.Monad (forM_)
-import Data.Maybe (fromJust)
-import Data.Text (Text, pack)
-import Control.Monad.IO.Class (MonadIO(liftIO))
-import Control.Monad.Logger (MonadLogger(..), runStderrLoggingT)
+import Control.Monad.IO.Class
+import Control.Monad.Logger (MonadLogger (..), runStderrLoggingT)
 import Control.Monad.Trans.Resource (MonadThrow, ResourceT, runResourceT)
-import Database.Esqueleto
-       ( SqlExpr
-       , Value(..)
-       , from
-       , select
-       , set
-       , unValue
-       , update
-       , val
-       , where_
-       , (=.)
-       , (^.)
-       )
-import Database.Persist
-import Database.Persist.Postgresql
-       ( ConnectionString
-       , SqlPersistT
-       , runMigration
-       , runSqlConn
-       , transactionUndo
-       , withPostgresqlConn
-       )
-import Database.Persist.TH
-       (mkMigrate, mkPersist, persistUpperCase, share, sqlSettings)
-import Data.Geospatial (PointXY(..))
+import Data.Geospatial (PointXY (..), PointXYZ (..), PointXYZM (..))
+import Data.LineString (LineString, makeLineString)
+import Data.LinearRing (LinearRing)
 import Data.List.NonEmpty (NonEmpty)
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
-import Data.LineString (LineString, makeLineString)
-import Data.LinearRing (LinearRing, makeLinearRing)
+import Database.Esqueleto.Experimental
+import Database.Esqueleto.Postgis
+import Database.Persist
+import Database.Persist.Postgresql
+  ( ConnectionString,
+    withPostgresqlConn,
+  )
+import Database.Persist.TH
+  ( mkMigrate,
+    mkPersist,
+    persistUpperCase,
+    share,
+    sqlSettings,
+  )
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.HUnit
 
 connString :: ConnectionString
 connString = "host=localhost port=5432 user=test dbname=test password=test"
 
 -- Test schema
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistUpperCase|
-  Unit
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistUpperCase|
+  Unit sql=unit
     geom       (PostgisGeometry PointXY)
+    deriving Eq Show
+
+  Unityz sql=unityz
+    geom       (PostgisGeometry PointXYZ)
+    deriving Eq Show
+
+  Unityzm sql=unityzm
+    geom       (PostgisGeometry PointXYZM)
     deriving Eq Show
 |]
 
-initializeDB
-  :: ( MonadIO m, MonadLogger m, MonadThrow m)
-  => SqlPersistT (ResourceT m) ()
-initializeDB  = do
+initializeDB ::
+  (MonadIO m) =>
+  SqlPersistT (ResourceT m) ()
+initializeDB = do
   runMigration migrateAll
 
-runDB :: (forall m . (MonadIO m, MonadLogger m, MonadThrow m) => SqlPersistT (ResourceT m) a) -> IO a
-runDB act
-  = runStderrLoggingT
-  . runResourceT
-  . withPostgresqlConn connString
-  . runSqlConn
-  $ (initializeDB >> act >>= \ret -> transactionUndo >> return ret)
+runDB :: (forall m. (MonadIO m, MonadLogger m, MonadThrow m) => SqlPersistT (ResourceT m) a) -> IO a
+runDB act =
+  runStderrLoggingT
+    . runResourceT
+    . withPostgresqlConn connString
+    . runSqlConn
+    $ (initializeDB >> act >>= \ret -> transactionUndo >> return ret)
 
 main :: IO ()
 main = defaultMain tests
@@ -95,52 +88,121 @@ tests = testGroup "Tests" [unitTests]
 
 test' :: Gen (PostgisGeometry PointXY) -> TestTree
 test' gen =
-    testCase ("List comparison (different length)") $ do
-        someUnit <- Gen.sample (Unit <$> gen)
-        result <- runDB $ do
-          insert someUnit
-          selectList @(Unit) [] []
-        (entityVal <$> result) @?= [someUnit]
+  testCase ("List comparison (different length)") $ do
+    someUnit <- Gen.sample (Unit <$> gen)
+    result <- runDB $ do
+      _ <- insert someUnit
+      selectList @(Unit) [] []
+    (entityVal <$> result) @?= [someUnit]
+
+testxyz :: Gen (PostgisGeometry PointXYZ) -> TestTree
+testxyz gen =
+  testCase ("List comparison (different length)") $ do
+    someUnit <- Gen.sample (Unityz <$> gen)
+    result <- runDB $ do
+      _ <-  insert someUnit
+      selectList @(Unityz) [] []
+    (entityVal <$> result) @?= [someUnit]
+
+testxyzm :: Gen (PostgisGeometry PointXYZM) -> TestTree
+testxyzm gen =
+  testCase ("List comparison (different length)") $ do
+    someUnit <- Gen.sample (Unityzm <$> gen)
+    result <- runDB $ do
+      _ <- insert someUnit
+      selectList @(Unityzm) [] []
+    (entityVal <$> result) @?= [someUnit]
 
 unitTests :: TestTree
-unitTests = testGroup "Unit tests" $
-   (test') <$> (genCollection : genGeometry)
+unitTests =
+  testGroup
+    "Unit tests"
+    [ testGroup "roundtrip tests xy" $
+        (test') <$> (genCollection genPointxy : genGeometry genPointxy),
+      testGroup "roundtrip tests xyz" $
+        (testxyz) <$> (genCollection genPointxyz : genGeometry genPointxyz),
+      testGroup "roundtrip tests xyzm" $
+        (testxyzm) <$> (genCollection genPointxyzm : genGeometry genPointxyzm),
+      testGroup "function bindings" $
+        [ testCase ("it finds the one unit with st_contains") $ do
+            result <- runDB $ do
+              _ <- insert $
+                Unit
+                  { unitGeom = Polygon $ makePolygon (PointXY 0 0) (PointXY 0 2) (PointXY 2 2) $ Seq.fromList [(PointXY 2 0)]
+                  }
+
+              selectOne $ do
+                unit <- from $ table @Unit
+                where_ $ unit ^. UnitGeom `st_contains` (val $ Point (PointXY 1 1))
+                pure countRows
+            unValue <$> result @?= (Just (1 :: Int)),
+          testCase ("it finds the one unit with intersection st_point") $ do
+            result <- runDB $ do
+              _ <- insert $
+                Unit
+                  { unitGeom = Polygon $ makePolygon (PointXY 0 0) (PointXY 0 2) (PointXY 2 2) $ Seq.fromList [(PointXY 2 0)]
+                  }
+
+              selectOne $ do
+                unit <- from $ table @Unit
+                where_ $ unit ^. UnitGeom `st_contains` (st_point (val 1) (val 1))
+                pure countRows
+            unValue <$> result @?= (Just (1 :: Int)),
+          testCase ("it finds the one unit with intersection st_intersects") $ do
+            result <- runDB $ do
+              _ <- insert $
+                Unit
+                  { unitGeom = Polygon $ makePolygon (PointXY 0 0) (PointXY 0 2) (PointXY 2 2) $ Seq.fromList [(PointXY 2 0)]
+                  }
+
+              selectOne $ do
+                unit <- from $ table @Unit
+                where_ $ unit ^. UnitGeom `st_intersects` (st_point (val 1) (val 1))
+                pure countRows
+            unValue <$> result @?= (Just (1 :: Int))
+        ]
+    ]
 
 genDouble :: Gen Double
 genDouble = Gen.double (Range.exponentialFloat (-10) 10)
 
-genPoint :: Gen PointXY
-genPoint = PointXY <$> genDouble <*> genDouble
+genPointxy :: Gen PointXY
+genPointxy = PointXY <$> genDouble <*> genDouble
 
-genPoints :: Gen (NonEmpty PointXY)
-genPoints = Gen.nonEmpty (Range.constant 1 10) genPoint
+genPointxyz :: Gen PointXYZ
+genPointxyz = PointXYZ <$> genDouble <*> genDouble <*> genDouble
 
-genSeq :: Gen (Seq PointXY)
-genSeq = Seq.fromList <$> Gen.list (Range.constant 0 10) genPoint
+genPointxyzm :: Gen PointXYZM
+genPointxyzm = PointXYZM <$> genDouble <*> genDouble <*> genDouble <*> genDouble
 
-genLineString :: Gen (LineString PointXY)
-genLineString = makeLineString <$> genPoint <*> genPoint <*> genSeq
+genPoints :: Gen a -> Gen (NonEmpty a)
+genPoints genPoint = Gen.nonEmpty (Range.constant 1 10) genPoint
 
-genMultiLineString :: Gen (NonEmpty (LineString PointXY))
-genMultiLineString = Gen.nonEmpty (Range.constant 1 10) genLineString
+genSeq :: Gen a -> Gen (Seq a)
+genSeq genPoint = Seq.fromList <$> Gen.list (Range.constant 0 10) genPoint
 
-genLinearring :: Gen (LinearRing PointXY)
-genLinearring = makePolygon <$> genPoint <*> genPoint <*> genPoint <*> genSeq
+genLineString :: Gen a -> Gen (LineString a)
+genLineString genPoint = makeLineString <$> genPoint <*> genPoint <*> genSeq genPoint
 
-genMultiLinearring :: Gen (NonEmpty (LinearRing PointXY))
-genMultiLinearring = Gen.nonEmpty (Range.constant 1 10) genLinearring
+genMultiLineString :: Gen a -> Gen (NonEmpty (LineString a))
+genMultiLineString genPoint = Gen.nonEmpty (Range.constant 1 10) (genLineString genPoint)
 
-genCollection :: Gen (PostgisGeometry PointXY)
-genCollection = Collection <$> Gen.nonEmpty (Range.constant 1 10) (Gen.choice genGeometry)
+genLinearring :: (Eq a, Show a) => Gen a -> Gen (LinearRing a)
+genLinearring genPoint = makePolygon <$> genPoint <*> genPoint <*> genPoint <*> genSeq genPoint
 
-genGeometry :: [Gen (PostgisGeometry PointXY)]
-genGeometry =
-  [
-    -- pure NoGeometry
-     (Point <$> genPoint ),
-     (MultiPoint <$> genPoints),
-     (Line <$> genLineString),
-     (Multiline <$> genMultiLineString),
-     (Polygon <$> genLinearring),
-     (MultiPolygon <$> genMultiLinearring)
-             ]
+genMultiLinearring :: (Eq a, Show a) => Gen a -> Gen (NonEmpty (LinearRing a))
+genMultiLinearring genPoint = Gen.nonEmpty (Range.constant 1 10) (genLinearring genPoint)
+
+genCollection :: (Eq a, Show a) => Gen a -> Gen (PostgisGeometry a)
+genCollection genPoint = Collection <$> Gen.nonEmpty (Range.constant 1 10) (Gen.choice (genGeometry genPoint))
+
+genGeometry :: (Eq a, Show a) => Gen a -> [Gen (PostgisGeometry a)]
+genGeometry genPoint =
+  [ -- pure NoGeometry
+    (Point <$> genPoint),
+    (MultiPoint <$> genPoints genPoint),
+    (Line <$> genLineString genPoint),
+    (Multiline <$> genMultiLineString genPoint),
+    (Polygon <$> genLinearring genPoint),
+    (MultiPolygon <$> genMultiLinearring genPoint)
+  ]
